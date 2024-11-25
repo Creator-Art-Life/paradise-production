b5b3079538cfbf8b59a0af36e07f4e0638798fa2
Creator-Art-Life <maximshool12@gmail.com>
2024-11-25 21:37:54 +0200

    bubububub

--- src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/_components/pipeline-view.tsx
@@ -161,13 +161,13 @@ const PipelineView = ({
               <div className="flex mt-4">
                 {allLanes.map((lane, index) => (
                   <PipelineLane
-                    //@ts-ignore
+                    //@ts-expect-error some text for resolve
                     allTickets={allTickets}
-                    //@ts-ignore
+                    //@ts-expect-error some text for resolve
                     setAllTickets={setAllTickets}
                     subaccountId={subaccountId}
                     pipelineId={pipelineId}
-                    //@ts-ignore
+                    //@ts-expect-error some text for resolve
                     tickets={lane.Tickets}
                     laneDetails={lane}
                     index={index}
