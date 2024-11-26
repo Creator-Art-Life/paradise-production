b49a9f5e9681288ecaca93b4ebf32f2b6dbd0802
Creator-Art-Life <maximshool12@gmail.com>
2024-11-26 10:10:20 +0200

    fix

--- src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
       <div className="flex flex-col items-center justify-start">
         <div className="mt-20 text-center">
           <p className="text-2xl italic font-bold">Maybe when I have time...</p>
-          <p className="text-lg mt-2 font-bold">I'll do this page</p>
+          <p className="text-lg mt-2 font-bold">I will do this page</p>
         </div>
       </div>
     </BlurPage>
